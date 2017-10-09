ca927d140c8038ef9a94277debaf689f5e566806
hnae6443 <hnae6443@gmail.com>
2017-10-09 19:16:50 -0400

    Used consts for props names

--- static/scripts/TodoItem.js
+++ static/scripts/TodoItem.js
@@ -24,31 +24,33 @@ class TodoItem extends React.Component {
   }
 
   render() {
+    const { id, done, content } = this.props;
+
     return (
-      <div className="todo-card" key={this.props.id}>
+      <div className="todo-card" key={id}>
         <div className="todo-content">
           <input
             className="todo-done"
             type="checkbox"
-            onChange={() => this.handleCheckbox(this.props.id)}
-            checked={this.props.done}
+            onChange={() => this.handleCheckbox(id)}
+            checked={done}
           />
-        <p className={"todo-text " + (this.props.done ? 'completed' : '')}>{this.props.content}</p>
+        <p className={"todo-text " + (done ? 'completed' : '')}>{content}</p>
         </div>
         <div className="todo-buttons">
           <div className="order-buttons">
-            <a className="move-up" onClick={() => this.props.moveUp(this.props.id)}>
+            <a className="move-up" onClick={() => this.props.moveUp(id)}>
               <svg className="svg-btn" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 306 306">
                 <path fill="#aaa" d="M270.3 58.6L153 176 35.7 58.6 0 94.3l153 153 153-153"/>
               </svg>
             </a>
-            <a className="move-down" onClick={() => this.props.moveDown(this.props.id)}>
+            <a className="move-down" onClick={() => this.props.moveDown(id)}>
               <svg className="svg-btn" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 306 306">
                 <path fill="#aaa" d="M270.3 58.6L153 176 35.7 58.6 0 94.3l153 153 153-153"/>
               </svg>
             </a>
           </div>
-          <a className="todo-remove" onClick={() => this.handleDelete(this.props.id)}>&#10005;</a>
+          <a className="todo-remove" onClick={() => this.handleDelete(id)}>&#10005;</a>
         </div>
       </div>
     );
